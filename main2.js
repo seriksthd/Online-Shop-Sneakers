1f9707095a57f1167f8ea1d59de31c2f7b4a4dbd
seriksthd <s29014517@gmail.com>
2024-12-02 02:30:40 +0300

    xs

---
+++ main2.js
@@ -0,0 +1,51 @@
+function toggleForm() {
+  const loginForm = document.getElementById("login-form");
+  const registerForm = document.getElementById("register-form");
+
+  loginForm.classList.toggle("hidden");
+  registerForm.classList.toggle("hidden");
+}
+
+document
+  .getElementById("register-form")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const name = document.getElementById("register-name").value;
+    const email = document.getElementById("register-email").value;
+    const password = document.getElementById("register-password").value;
+
+    if (name && email && password) {
+      const userData = { name, email, password };
+      localStorage.setItem("user", JSON.stringify(userData));
+      alert("Вы успешно зарегистрировались!");
+      toggleForm();
+    } else {
+      document.getElementById("register-error").textContent =
+        "Пожалуйста, заполните все поля.";
+    }
+  });
+
+document
+  .getElementById("login-form")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const email = document.getElementById("login-email").value;
+    const password = document.getElementById("login-password").value;
+
+    const storedUser = JSON.parse(localStorage.getItem("user"));
+
+    if (!storedUser) {
+      document.getElementById("login-error").textContent =
+        "Пользователь не найден. Сначала зарегистрируйтесь.";
+    } else {
+      if (email === storedUser.email && password === storedUser.password) {
+        confirm("Добро пожаловать, " + storedUser.name + "!");
+        window.location.href = "index.html";
+      } else {
+        document.getElementById("login-error").textContent =
+          "Неверный email или пароль.";
+      }
+    }
+  });
