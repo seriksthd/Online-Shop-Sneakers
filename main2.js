7a8401066860a230935ca5fc2de9e1d63cc7e918
seriksthd <s29014517@gmail.com>
2024-12-03 00:41:18 +0300

    xs

--- main2.js
+++ main2.js
@@ -42,7 +42,7 @@ document
     } else {
       if (email === storedUser.email && password === storedUser.password) {
         confirm("Добро пожаловать, " + storedUser.name + "!");
-        window.location.href = "index2.html";
+        window.location.href = "index4.html";
       } else {
         document.getElementById("login-error").textContent =
           "Неверный email или пароль.";
